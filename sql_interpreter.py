e23cf7a9ad12be9ab6f8295310497ebd1dd40285
LachlanJW <161661313+LachlanJW@users.noreply.github.com>
2024-04-08 10:04:41 +1000

    Some comments added to improve readability

--- sql_interpreter.py
+++ sql_interpreter.py
@@ -20,48 +20,57 @@ sql_string = f"mysql+mysqlconnector://root:{SQL_PASSWORD}@localhost:3306/{DATABA
 engine = create_engine(sql_string)  # Set echo=True to print to console
 
 
-def create_table_from_json(data: List[Dict]) -> None:
-    """Create an SQL table from JSON data"""
+def create_table_from_json(data: List[Dict], table: str = 'houses') -> None:
+    """Create an SQL table from JSON data.
+    Args: List[Dict] given from json.load.
+    Returns: None. The data is written to a local sql table"""
     try:
         # Normalize JSON data and create a DataFrame
         normalized_data = pd.json_normalize(data)
         df = pd.DataFrame(normalized_data)
 
-        # Perform some data cleaning, type conversion etc
-        # First, convert some columns to integer, some to float
-        int_list = ["id",
-                    "address.postcode",
-                    "features.beds",
-                    "features.baths",
-                    "features.parking",
-                    "features.landSize"
-                    ]
-        for column in int_list:  # Fill NA values as 0
-            df[column] = df[column].fillna(0).astype(int)
-
-        df["address.lat"] = df["address.lat"].fillna(0).astype(float)
-        df["address.lng"] = df["address.lng"].fillna(0).astype(float)
-
-        # Then some string parsing
-        df['price'] = df['price'].str.replace('$', '').str.replace(',', '')
-        df['price'] = df['price'].astype(int)
-        df.loc[df['features.propertyType'] == 'ApartmentUnitFlat',
-               'features.propertyType'] = 'Apartment'
-
-        # Finally, remove some irrelevant columns
-        df = df.drop(columns=[
-            'features.propertyTypeFormatted',
-            'features.landUnit',
-            'features.isRural',
-            'features.isRetirement'
-            ])
+        df = clean_df(df)  # Perform some data cleaning, type conversion etc
 
         # Write DataFrame to SQL table
-        df.to_sql(name='houses', con=engine,
+        df.to_sql(name=table, con=engine,
                   if_exists='replace', index=False)
         print("Successfully written to sql table")
     except SQLAlchemyError as e:
         print(f"An error occurred while creating the table: {e}")
+
+
+def clean_df(df: pd.DataFrame) -> pd.DataFrame:
+    """Perform some data cleaning specific to this dataframe.
+    Args and Returns: pd.Dataframe."""
+    # First, convert some columns to integer, some to float
+    int_list = ["id",
+                "address.postcode",
+                "features.beds",
+                "features.baths",
+                "features.parking",
+                "features.landSize"
+                ]
+    for column in int_list:  # Fill NA values as 0
+        df[column] = df[column].fillna(0).astype(int)
+
+    df["address.lat"] = df["address.lat"].fillna(0).astype(float)
+    df["address.lng"] = df["address.lng"].fillna(0).astype(float)
+
+    # Then some string parsing
+    df['price'] = df['price'].str.replace('$', '').str.replace(',', '')
+    df['price'] = df['price'].astype(int)
+    df.loc[df['features.propertyType'] == 'ApartmentUnitFlat',
+           'features.propertyType'] = 'Apartment'
+
+    # Finally, remove some irrelevant columns
+    df = df.drop(columns=[
+        'features.propertyTypeFormatted',
+        'features.landUnit',
+        'features.isRural',
+        'features.isRetirement'
+        ])
+
+    return df
 
 
 def get_data_from_sql(table: str) -> Sequence:
